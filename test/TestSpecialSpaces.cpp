ce6422b77f0202647ec72520d2a45bce5b5029cd
jemeador <jemeador@gmail.com>
2021-02-16 01:05:30 -0600

    Added more income tax tests

--- test/TestSpecialSpaces.cpp
+++ test/TestSpecialSpaces.cpp
@@ -8,7 +8,7 @@ using namespace monopoly;
 SCENARIO("When a player lands on Income Tax, they pay the lesser of $200 or 10% of their net worth", "[special]") {
 	Test test;
 
-	GIVEN("Player 1's net worth is less than $2000") {
+	GIVEN("Player 1's funds are less than $2000") {
 		int const startingFunds = 1990;
 		test.set_player_funds(Player::p1, startingFunds);
 		test.move_player(Player::p1, Space::Go);
@@ -22,7 +22,7 @@ SCENARIO("When a player lands on Income Tax, they pay the lesser of $200 or 10% 
 		}
 	}
 
-	GIVEN("Player 1's net worth is more than $2000") {
+	GIVEN("Player 1's funds are more than $2000") {
 		int const startingFunds = 2010;
 		test.set_player_funds(Player::p1, startingFunds);
 		test.move_player(Player::p1, Space::Go);
@@ -33,6 +33,56 @@ SCENARIO("When a player lands on Income Tax, they pay the lesser of $200 or 10% 
 			THEN("player 1 loses $200 of their funds") {
 				test.require_funds(Player::p1, startingFunds - 200);
 			}
+		}
+	}
+
+	GIVEN("Player 1's has 5 blue buildings, 2 mortgaged railroads") {
+		test.give_deeds(Player::p1, { Property::Blue_1, Property::Blue_2, Property::Railroad_1, Property::Railroad_2 });
+		test.set_buildings({
+			{ Property::Blue_1, 2 },
+			{ Property::Blue_2, 3 },
+            });
+		test.mortgage_properties({ Property::Railroad_1, Property::Railroad_2 });
+
+		AND_GIVEN("player 1 has $21") {
+            int const startingFunds = 21;
+            test.set_player_funds(Player::p1, startingFunds);
+
+            WHEN("player 1 lands on Income Tax") {
+                test.land_on_space(Player::p1, Space::IncomeTax);
+
+                THEN("player 1 loses %10 of their net worth") {
+                    // Blue 1 @ 350
+                    // Blue 2 @ 400
+                    // 4th row buildings @ $200 x 5
+                    // Mortgaged railroads @ 100 x 2
+                    // Cash on hand 21
+                    //
+                    // Total = 1971
+                    // Tax = 197.1, rounded up to 198
+                    test.require_funds(Player::p1, startingFunds - 198);
+                }
+            }
+		}
+		AND_GIVEN("player 1 has $200") {
+            int const startingFunds = 200;
+            test.set_player_funds(Player::p1, startingFunds);
+
+            WHEN("player 1 lands on Income Tax") {
+                test.land_on_space(Player::p1, Space::IncomeTax);
+
+                THEN("player 1 loses $200") {
+                    // Blue 1 @ 350
+                    // Blue 2 @ 400
+                    // 4th row buildings @ $200 x 5
+                    // Mortgaged railroads @ 100 x 2
+                    // Cash on hand 200
+                    //
+                    // Total = 2150
+                    // Tax = 215, capped at 200
+                    test.require_funds(Player::p1, startingFunds - 200);
+                }
+            }
 		}
 	}
 }
